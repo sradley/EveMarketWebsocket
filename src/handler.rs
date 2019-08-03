975879abf8b7feb81b6c710f72dfa0eee525ea5f
sradley <stephen.t.radley@protonmail.com>
2019-08-03 17:32:38 +1000

    moved socket handler code to lib

--- src/handler.rs
+++ src/handler.rs
@@ -3,50 +3,8 @@
 //! ...
 
 use std::sync::Arc;
-use ws::{Handler, Sender};
-use log::{info, warn, debug};
 use crate::worker::WorkerHandler;
 use crate::ClientList;
-
-/// `SocketHandler` struct ...
-pub struct SocketHandler {
-    clients: ClientList,
-    conn: Arc<Sender>,
-}
-
-impl SocketHandler {
-    /// `SocketHandler` constructor ...
-    pub fn new(clients: ClientList, conn: Arc<Sender>) -> Self {
-        Self { clients, conn }
-    }
-}
-
-impl Handler for SocketHandler {
-    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
-        info!("[websocket] client connected from: {:?}", shake.peer_addr.unwrap());
-
-        self.conn.send("Welcome to EveMarketAnalysis v0.1.0").unwrap();
-        let mut clients = self.clients.lock().unwrap();
-        clients.insert(self.conn.token(), Arc::clone(&self.conn));
-
-        debug!("[websocket] clients connected `{}`", clients.len());
-
-        Ok(())
-    }
-
-    fn on_close(&mut self, code: ws::CloseCode, reason: &str) {
-        info!("[websocket] client disconnected ({:?}) {}", code, reason);
-
-        let mut clients = self.clients.lock().unwrap();
-        clients.remove(&self.conn.token());
-
-        debug!("[websocket] clients connected `{}`", clients.len());
-    }
-
-    fn on_error(&mut self, _: ws::Error) {
-        warn!("[websocket] error occurred in websocket");
-    }
-}
 
 /// `MarketHandler` struct ...
 pub struct MarketHandler {
--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,9 @@ pub mod worker;
 
 use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
-use log::{Record, Metadata, info};
+use log::{Record, Metadata, info, warn, debug};
 use chrono::Utc;
-use ws::Sender;
+use ws::{Sender, Handler};
 use ws::util::Token;
 
 /// `ClientList` type ...
@@ -45,6 +45,46 @@ pub fn run(region_ids: Vec<i32>, ip: &str, port: i32) -> ws::Result<()> {
     handler.start();
     
     ws::listen(format!("{}:{}", ip, port), move |out| {
-        handler::SocketHandler::new(Arc::clone(handler.clients()), Arc::new(out))
+        SocketHandler::new(Arc::clone(handler.clients()), Arc::new(out))
     })
+}
+
+/// `SocketHandler` struct ...
+pub struct SocketHandler {
+    clients: ClientList,
+    conn: Arc<Sender>,
+}
+
+impl SocketHandler {
+    /// `SocketHandler` constructor ...
+    pub fn new(clients: ClientList, conn: Arc<Sender>) -> Self {
+        Self { clients, conn }
+    }
+}
+
+impl Handler for SocketHandler {
+    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
+        info!("[websocket] client connected from: {:?}", shake.peer_addr.unwrap());
+
+        self.conn.send("Welcome to EveMarketAnalysis v0.1.0").unwrap();
+        let mut clients = self.clients.lock().unwrap();
+        clients.insert(self.conn.token(), Arc::clone(&self.conn));
+
+        debug!("[websocket] clients connected `{}`", clients.len());
+
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: ws::CloseCode, reason: &str) {
+        info!("[websocket] client disconnected ({:?}) {}", code, reason);
+
+        let mut clients = self.clients.lock().unwrap();
+        clients.remove(&self.conn.token());
+
+        debug!("[websocket] clients connected `{}`", clients.len());
+    }
+
+    fn on_error(&mut self, _: ws::Error) {
+        warn!("[websocket] error occurred in websocket");
+    }
 }
